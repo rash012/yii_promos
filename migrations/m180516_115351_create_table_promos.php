f6b45e597db6f3279d98c9f9e5b72f649773b7c6
rash012 <rash012@mail.ru>
2018-05-16 15:07:42 +0300

    init yii basic

---
+++ migrations/m180516_115351_create_table_promos.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180516_115351_create_table_promos
+ */
+class m180516_115351_create_table_promos extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180516_115351_create_table_promos cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180516_115351_create_table_promos cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
